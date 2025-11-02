a94c387d8b36b0fd0fec445657b208b4dbe6a29e
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-02 12:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const selectList = document.querySelector("#colorSelect");
+const input = document.querySelector("input");
+let colors = ["Red", "Green", "White", "Black"];
+
+function renderList() {
+  selectList.innerHTML = colors
+    .map((color) => {
+      return `<option>${color}</option>`;
+    })
+    .join("");
+}
+
+input.addEventListener("click", () => {
+  const value = selectList.value;
+  colors = colors.filter((color) => color != value);
+  renderList();
+});
+
+renderList();
